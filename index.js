06f28255e855761e1500b3de79ce62881e3122fc
Ananya Upadhyay <ananyaupadhyay.contact@gmail.com>
2021-05-28 16:49:10 +0530

    index updated

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("hello world");
+// let and const
+// scope of variables 
+var personName="ananya"
+console.log(personName)
+// var creates directly with global scope
+
+// fuctions and arrow function
+function sum(num1,num2)
+{
+    return num1+num2;
+}
+sum(30,50)
+let result =sum(30,50);
+console.log(result);
+sum=(num1,num2)=>{
+    return num1+num2;
+
+}
+console.log(result);
+
+
---
+++ json.js
@@ -0,0 +1,12 @@
+// json
+var myDetails=
+{
+name:"ananya",
+age= 20,
+bloodgroup="b+"
+}
+JSON.Parse('{'key1': 'value1', 'key2': 20}')
+JSON.stringify(myDetails)
+console.log(myDetails);
+console.log(myDetails.age);
+console.log(myDetails['bloodgroup']);
