5b312dab3d04229e4e39ee9d30da50d07c2b65cb
Ananya Upadhyay <ananyaupadhyay.contact@gmail.com>
2021-06-01 11:55:39 +0530

    index.js modified

--- index.js
+++ index.js
@@ -18,5 +18,21 @@ sum=(num1,num2)=>{
 
 }
 console.log(result);
+// destructuring objects
+const player =
+{
+    name: " Lebron James" ,
+    club:  'la Lakers',
+    address:
+    {
+        city: ' los Angeles'
+        
+    }
+};
+// same console.log(player.address.city);
+ const { name, club , address:{city}} = player;
 
+ //console.log(`${name} plays for ${club}`);
+ console.log(`${name}lives in ${city}`);
+//object structuring allows you to grab these keys without having access to all the time the object name
 
